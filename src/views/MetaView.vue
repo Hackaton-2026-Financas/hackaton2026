aaffabbb68173c4bc9f8da2be88279cf87c97a2d
ArthursSchmidt <arthurifc0@gmail.com>
2026-07-07 11:59:35 -0300

    comit inicial

--- src/views/MetaView.vue
+++ src/views/MetaView.vue
@@ -2,8 +2,34 @@
 </script>
 
 <template>
+    <div class="fábrica">
+        <h2>
+            Fábrica de Metas🎯
+        </h2>
+        <p>
+            Crie e acompanhe seus objetivos financeiro aqui
+        </p>
+        <button>
+            + Nova meta
+        </button>
 
+    </div>
+    <div class="metas">
+        <h3>
+            Nehuma meta criada ainda
+        </h3>
+        <p>
+            Comece criando sua primeira meta financeira!
+        </p>
+        <button>
+            + Criar primeira meta
+        </button>
+    </div>
 </template>
 
 <style scoped>
+    div.metas{
+        background-color: whio;
+        color: black;
+    }
 </style>
