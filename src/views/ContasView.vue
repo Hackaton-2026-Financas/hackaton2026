68b9721cc3dc0635ba6f994c0bdb92ee407efbc5
camillyvitoria16 <millyvitoriaifc@gmail.com>
2026-08-11 11:59:18 -0300

    Inicio pag Contas Hackthon 2026

--- src/views/ContasView.vue
+++ src/views/ContasView.vue
@@ -1,8 +1,65 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
+  <section>
+    <h1>Gerenciador de Contas</h1>
+    <p>Arraste e solte suas contas para organizá-las</p>
+    <button>+ Nova Conta</button>
+  </section>
+  <section>
+    <img src="" alt="" />
+    <h1>A Pagar</h1>
+    <p>2 Contas</p>
+    <br />
+    <p>Total</p>
+    <h2>R$ 249,90</h2>
+    <div class="card1">
+      <h2>Energia Elétrica</h2>
+      <p>Utilidades</p>
+      <h3>$</h3>
+      <h3>R$ 150,00</h3>
+      <img src="" alt="" />
+      <h3>24/06/2026(Atrasada!)</h3>
+      <img src="" alt="" />
+    </div>
 
+    <div class="card2">
+      <h1>Internet</h1>
+      <p>Utilidades</p>
+      <h3>$</h3>
+      <h3>R$ 99,90</h3>
+      <img src="" alt="" />
+      <h3>19/06/2026(Atrasada!)</h3>
+      <img src="" alt="" />
+    </div>
+</section>
+<section>
+    <img src="" alt="">
+    <h1>Pagas</h1>
+    <p>1 Conta</p>
+    <br>
+    <p>Total</p>
+    <h2>R$ 1.200,00</h2>
+    <div class="card3">
+    <h1>Aluguel</h1> 
+    <p>Moradia</p>
+    <h3>$</h3>
+    <h3>R$ 1.200,00</h3>
+    <img src="" alt="">
+    <h3>09/06/2026</h3>
+    <img src="" alt="">
+    </div>
+
+
+
+
+
+</section>
 </template>
 
 <style scoped>
+
+
+
+
+
 </style>
